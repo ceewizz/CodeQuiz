afe8473f75c76ef9adb8a0be186f764f20c2ab6d
ceewizz <ceewizzhuhwin@gmail.com>
2023-10-30 13:47:56 -0700

    Code Quiz

--- assets/questions.js
+++ assets/questions.js
@@ -1,11 +1,163 @@
 // list of all questions, choices, and answers
 var questions = [
     {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: ["numbers and strings", 
+      "other arrays", 
+      "booleans", 
+      "all of the above"],
+      answer: "all of the above"
+    },
+    {
       title: "Commonly used data types DO NOT include:",
       choices: ["strings", 
       "booleans", 
       "alerts", 
       "numbers"],
       answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed with ____.",
+      choices: ["quotes", 
+      "curly brackets", 
+      "parentheses", 
+      "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: ["numbers and strings", 
+      "other arrays", 
+      "booleans", 
+      "all of the above"],
+      answer: "all of the above"
+    },
+    {
+      title: "Given a binary search tree, which traversal type would print the values in the nodes in sorted order ?",
+      choices: ["Inorder", 
+      "Preorder", 
+      "Postorder", 
+      "Level order"],
+      answer: "Inorder"
+    },
+    {
+      title: "Which of the following is NOT a valid JavaScript identifier?",
+      choices: ["var", 
+              "let", 
+              "const", 
+              "function"],
+      answer: "function"
+    },
+    {
+      title: "Which of the following statements is correct about the partition step?"
+      choices: ["The pivot element is chosen randomly", 
+              "The pivot element is chosen in the middle of the array", 
+              "The pivot element is chosen at the end of the array", 
+              "The pivot element is chosen at the beginning of the array"],
+      answer: "The pivot element is chosen randomly"
+    },
+    {
+      title: "Which of the following statements about binary trees is NOT true?"
+      choices: ["A binary tree is a tree in which each node has at most two children", 
+              "A binary tree is a tree in which each node has at most three children", 
+              "A binary tree is a tree in which each node has at most four children", 
+              "A binary tree is a tree in which each node has at most five children"],
+      answer: "A binary tree is a tree in which each node has at most four children"
+    },
+    {
+      title: "What is the static type of variable b after line 4 has been executed and before line 5 is executed?",
+      choices: ["boolean", 
+              "number", 
+              "string", 
+              "undefined"],
+      answer: "undefined"
+    },
+    {
+      title: "Let x be an integer which can take a value of 0 or 1. The statement if(x = =0) x = 1; else x = 0; is equivalent to which one of the following?",
+      choices: ["x = 1", 
+              "x = 0", 
+              "x = 1", 
+              "x = 0"],
+      answer: "x = 1"
+    },
+    {
+      title: "What tag is used to specify a section of text that has been quoted from another source?",
+      choices: ["<q>", 
+              "<blockquote>", 
+              "<cite>", 
+              "<q>"],
+      answer: "<q>"
+    },
+    {
+      title: "What tag is used to define an image or add an image to an HTML page?",
+      choices: ["<img>", 
+              "<picture>", 
+              "<img src>", 
+              "<img srcset>"],
+      answer: "<img>"
+    },
+    {
+      title: "Which of the following is NOT a valid HTML tag?",
+      choices: ["<q>", 
+              "<blockquote>", 
+              "<cite>", 
+              "<q>"],
+      answer: "<q>"
+    },
+    {
+      title: "What tag is used to render or transform text into an emphasized (italics) version?",
+      choices: ["<em>", 
+              "<i>", 
+              "<strong>", 
+              "<em>"],
+      answer: "<em>"
+    },
+    {
+      title: "What tag is used to define a standard cell inside a table?" ,
+      choices: ["<td>", 
+              "<th>", 
+              "<tr>", 
+              "<td>"],
+      answer: "<td>"
+    },
+    {
+      title: "What tag can be used to insert a line break or blank line in an HTML document?",
+      choices: ["<br>", 
+              "<hr>", 
+              "<br />", 
+              "<br>"],
+      answer: "<br>"
+    },
+    {
+      title:"What tag is used to define the metadata about an HTML document, and must always be included inside the element?",
+      choices: ["<meta>", 
+      "<head>", 
+      "<body>", 
+      "<html>"],
+      answer: "<html>"
+    },
+    {
+      title: "What tag is used to define an unordered list that is bulleted?",
+      choices: ["<ul>", 
+              "<ol>", 
+              "<li>", 
+              "<ul>"],
+      answer: "<ul>"
+    },
+    {
+      title: "What tag is used to define an ordered list that is numbered?",
+      choices: ["<ol>", 
+              "<ul>", 
+              "<li>", 
+              "<ol>"],
+              answer: "<ol>"
+    },
+    {
+      title: "What tag is used to define the bottom section (footer) of an HTML document?",
+      choices: ["<footer>", 
+              "<body>", 
+              "<html>", 
+              "<footer>"],
+      answer: "<footer>"
     }
   ];
